ebbdc92e1834bbc59ca74b8db1d886a46b2e4b3f
Aniket Das <aniketdas001@gmail.com>
2024-05-11 15:23:13 -0700

    frontend: replace hardcoded text with state text

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,27 +14,61 @@ import {
 } from "mdb-react-ui-kit";
 
 export default function App() {
-  const [texts, setTexts] = useState("Word")
+  const [texts, setTexts] = useState([
+    {
+      user: 'Socrates',
+      text: 'The only true wisdom is in knowing you know nothing.'
+    },
+    {
+      user: 'Aristotle',
+      text: 'It is the mark of an educated mind to be able to entertain a thought without accepting it.'
+    },
+    {
+      user: 'Lawyer',
+      text: 'The more you know, the more you realize you know nothing.'
+    },
+    {
+      user: 'Socrates',
+      text: 'The only true wisdom is in knowing you know nothing.'
+    },
+  ])
+  const [agents, setAgents] = useState([
+    'Socrates', 'Lawyer', 'Aristotle'
+  ])
+  const [discussionStarted, setDiscussionStarted] = useState(false)
+
+  const startDiscussion = () => {
+    setDiscussionStarted(true)
+  }
   return (
     <MDBContainer fluid className="py-5 gradient-custom">
-      <MDBRow>
-        <MDBTypography>
-          <h1>
+      <MDBRow className="text-center mb-3">
+        <h1 >
           LlamaSage
-          </h1>
+        </h1>
+      </MDBRow>
+      <MDBRow className="mb-3">
+          <MDBCol size='9'>
+              <MDBTextArea className="mb-3" label="Dilemma to ponder" id="textAreaExample" rows={4} />
+          </MDBCol>
+          <MDBCol size='3'>
+            <h5>Select agents for participation</h5>
+            <MDBCheckbox label="Socrates" id="checkbox1" />
+            <MDBCheckbox label="Lawyer" id="checkbox1" />
+            <MDBCheckbox label="Aristotle" id="checkbox1" />
+          </MDBCol>
+      </MDBRow>
+      <MDBRow className="text-center mb-3">
+        <MDBTypography>
+          <MDBBtn color="light" size="lg" rounded onClick={event => startDiscussion()}>
+            Start Discussion
+          </MDBBtn>
         </MDBTypography>
       </MDBRow>
-      <MDBRow>
-        <MDBTypography>
-            <MDBTextArea className="mb-3" label="Philosophical question to ponder" id="textAreaExample" rows={4} />
-            <MDBBtn color="light" size="lg" rounded className="float-end mb3">
-              Start Discussion
-            </MDBBtn>
-          </MDBTypography>
-      </MDBRow>
-      <MDBRow>
+      {<MDBRow>
           <MDBTypography>
-            <li className="d-flex justify-content-between mb-4">
+          {discussionStarted && texts.map((value, index) => {
+            return <li className="d-flex justify-content-between mb-4" key={index}>
               <img
                 src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp"
                 alt="avatar"
@@ -44,49 +78,19 @@ export default function App() {
               <MDBCard className="w-100 mask-custom">
                 <MDBCardHeader
                   className="d-flex justify-content-between p-3"
-                  style={{ borderBottom: "1px solid rgba(255,255,255,.3)" }}
                 >
-                  <p className="fw-bold mb-0">Brad Pitt</p>
-                  <p className="small mb-0">
-                    <MDBIcon far icon="clock" /> 12 mins ago
-                  </p>
+                  <p className="fw-bold mb-0">{value.user}</p>
                 </MDBCardHeader>
                 <MDBCardBody>
                   <p className="mb-0">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua.
+                    {value.text}
                   </p>
                 </MDBCardBody>
               </MDBCard>
             </li>
-            <li class="d-flex justify-content-between mb-4">
-              <img
-                src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-5.webp"
-                alt="avatar"
-                className="rounded-circle d-flex align-self-start me-3 shadow-1-strong"
-                width="60"
-              />
-              <MDBCard className="w-100 mask-custom">
-                <MDBCardHeader
-                  className="d-flex justify-content-between p-3"
-                  style={{ borderBottom: "1px solid rgba(255,255,255,.3)" }}
-                >
-                  <p class="fw-bold mb-0">Lara Croft</p>
-                  <p class="small mb-0">
-                    <MDBIcon far icon="clock" /> 13 mins ago
-                  </p>
-                </MDBCardHeader>
-                <MDBCardBody>
-                  <p className="mb-0">
-                    Sed ut perspiciatis unde omnis iste natus error sit
-                    voluptatem accusantium doloremque laudantium.
-                  </p>
-                </MDBCardBody>
-              </MDBCard>
-            </li>
+          })}
           </MDBTypography>
-      </MDBRow>
+      </MDBRow>}
     </MDBContainer>
   );
 }
